64ce4d11e06b44996ee7b3ba1c3bece5b01ddfa1
drkchiloll <samwomack_@me.com>
2016-02-25 08:56:50 -0600

    added new component..the HarViewer (main)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.scss';
+import HarViewer from './components/Harviewer';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>React Deep Dive</h1>
+      <HarViewer/>
     );
   }
 };
